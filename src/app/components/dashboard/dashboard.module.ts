1b999f671ead65aea9b0d28077d51503080ea940
J-DK <dileepkumarjami@gmail.com>
2021-04-17 13:44:55 +0530

    feat: added attendance component to the dashboard

--- src/app/components/dashboard/dashboard.module.ts
+++ src/app/components/dashboard/dashboard.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard.component';
-import {EmployerCreditLimitComponent} from "./employer-credit-limit/employer-credit-limit.component";
+import { EmployerCreditLimitComponent } from "./employer-credit-limit/employer-credit-limit.component";
+import { AttendanceTodayComponent } from "./attendance-today/attendance-today.component";
 
 
 
 @NgModule({
-  declarations: [DashboardComponent, EmployerCreditLimitComponent],
+  declarations: [DashboardComponent, EmployerCreditLimitComponent, AttendanceTodayComponent],
   exports: [
     DashboardComponent
   ],
